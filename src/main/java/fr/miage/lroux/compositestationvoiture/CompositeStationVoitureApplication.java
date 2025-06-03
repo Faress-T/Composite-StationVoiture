0f40b256860ec8985e609926104ce2e3e1eeb283
Faress-T <tahiri.faress@outlook.com>
2025-06-03 16:40:32 +0200

    init project

---
+++ src/main/java/fr/miage/lroux/compositestationvoiture/CompositeStationVoitureApplication.java
@@ -0,0 +1,13 @@
+package fr.miage.lroux.compositestationvoiture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompositeStationVoitureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompositeStationVoitureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/miage/lroux/compositestationvoiture/CompositeStationVoitureApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.lroux.compositestationvoiture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompositeStationVoitureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
